d175600b20af2fdaa6aaf8f1dd4515f97987719c
Lorraine <limgytjd98@gmail.com>
2022-03-11 04:54:35 +0900

    Make session storage

---
+++ src/core/sessionStorage.ts
@@ -0,0 +1,29 @@
+interface Props {
+    getItem(key: string): string | null;
+    setItem(key: string, value: string): void;
+    removeItem(key: string): void;
+}
+
+export default abstract class Storage<T extends string> {
+    private readonly storage: Props;
+
+    public constructor(getStorage = (): Props => window.sessionStorage) {
+        this.storage = getStorage();
+    }
+
+    protected get(key: T): string | null {
+        return this.storage.getItem(key);
+    }
+
+    protected set(key: T, value: string): void {
+        this.storage.setItem(key, value);
+    }
+
+    protected clearItem(key: T): void {
+        this.storage.removeItem(key);
+    }
+
+    protected clearItems(keys: T[]): void {
+        keys.forEach((key) => this.clearItem(key));
+    }
+}
