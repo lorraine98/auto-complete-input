3baaf6bc74b3c87e14ee54c472aee6fd2fef3f8f
lorraine98 <hyosung0425@naver.com>
2022-03-11 19:53:34 +0900

    Install file loader for svg

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,36 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: path.join(__dirname, 'src', 'index'),
-    output: {
-        path: path.join(__dirname, 'dist'),
-        chunkFilename: '[name].js',
-    },
-    module: {
-        rules: [
-            {
-                test: /.(j|t)s$/,
-                include: [path.resolve(__dirname, 'src')],
-                exclude: [path.resolve(__dirname, 'node_modules')],
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    resolve: { extensions: ['.json', '.js', '.ts'] },
-    plugins: [
-        new HtmlWebpackPlugin({ template: './index.html' }),
-        new CleanWebpackPlugin(),
+  mode: "development",
+  entry: path.join(__dirname, "src", "index"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    chunkFilename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /.(j|t)s$/,
+        include: [path.resolve(__dirname, "src")],
+        exclude: [path.resolve(__dirname, "node_modules")],
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: ["file-loader"],
+      },
     ],
-    devtool: 'source-map',
+  },
+  resolve: { extensions: [".json", ".js", ".ts"] },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new CleanWebpackPlugin(),
+  ],
+  devtool: "source-map",
 };
